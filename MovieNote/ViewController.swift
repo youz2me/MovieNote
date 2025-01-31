943c3f68eb5110f25cf0b762f87675be9d5b5410
youz2me <kynhun20@gachon.ac.kr>
2025-01-31 20:20:50 +0900

    init: 프로젝트 생성

---
+++ MovieNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieNote
+//
+//  Created by YOUJIM on 1/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
