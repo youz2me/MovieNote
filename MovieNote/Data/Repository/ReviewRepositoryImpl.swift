4c32f8c3cf85cbfa6ed1e330e76fcf22cda7b514
youz2me <kynhun20@gachon.ac.kr>
2025-02-13 11:55:09 +0900

    Refactor: #3 - 리뷰 재반영

--- MovieNote/Data/Repository/ReviewRepositoryImpl.swift
+++ MovieNote/Data/Repository/ReviewRepositoryImpl.swift
@@ -17,7 +17,7 @@ class ReviewService: ReviewRepository {
     
     func fetchReview() -> AnyPublisher<[String : Review], any Error> {
         return Future { promise in
-            if let reviewList: [String: Review] = self.storage.getValue(forKey: String.reviewList) {
+            if let reviewList: [String: Review] = self.storage.getValue(forKey: Constant.reviewList) {
                 promise(.success(reviewList))
             } else {
                 promise(.success([:]))
@@ -28,9 +28,9 @@ class ReviewService: ReviewRepository {
     
     func recordReview(key: String, review: Review) -> AnyPublisher<Bool, any Error> {
         return Future { promise in
-            if var reviewList: [String: Review] = self.storage.getValue(forKey: String.reviewList) {
+            if var reviewList: [String: Review] = self.storage.getValue(forKey: Constant.reviewList) {
                 reviewList.updateValue(review, forKey: key)
-                self.storage.setValue(reviewList, forKey: String.reviewList)
+                self.storage.setValue(reviewList, forKey: Constant.reviewList)
                 
                 promise(.success(true))
             }
@@ -40,9 +40,9 @@ class ReviewService: ReviewRepository {
     
     func deleteReview(key: String) -> AnyPublisher<Bool, any Error> {
         return Future { promise in
-            if var reviewList: [String: Review] = self.storage.getValue(forKey: String.reviewList) {
+            if var reviewList: [String: Review] = self.storage.getValue(forKey: Constant.reviewList) {
                 reviewList.removeValue(forKey: key)
-                self.storage.setValue(reviewList, forKey: String.reviewList)
+                self.storage.setValue(reviewList, forKey: Constant.reviewList)
                 
                 promise(.success(true))
             }
---
+++ MovieNote/Infra/Utils/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  MovieNote
+//
+//  Created by YOUJIM on 2/13/25.
+//
+
+
+import Foundation
+
+enum Constant {
+    static let reviewList = "ReviewList"
+}
